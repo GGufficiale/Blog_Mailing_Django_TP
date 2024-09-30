97c3b9910258667cabb7b07164ab660f79ff6bb8
GGufficiale <germangranovskii@mail.ru>
2024-09-30 22:17:35 +0300

    Доработка блога

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from django.views.decorators.cache import cache_page
 
-from blog.views import BlogListView, BlogCreateView
+from blog.views import BlogListView, BlogCreateView, BlogDeleteView, BlogUpdateView, toggle_activity, BlogDetailView
 
 from blog.apps import BlogConfig
 
@@ -9,4 +9,9 @@ app_name = BlogConfig.name
 
 urlpatterns = [
     path('', BlogListView.as_view(), name='blog_list'),
+    path('view/<int:pk>/', BlogDetailView.as_view(), name='blog_detail'),
+    path('create/', BlogCreateView.as_view(), name='blog_create'),
+    path('update/<int:pk>/', BlogUpdateView.as_view(), name='blog_update'),
+    path('delete/<int:pk>/', BlogDeleteView.as_view(), name='blog_delete'),
+    path('activity/<int:pk>/', toggle_activity, name='toggle_activity'),
 ]
